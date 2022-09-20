84cc258369ddfbeea8fc6f8f57fbaac3f20d1895
Yamzx14 <99759853+Yamzx14@users.noreply.github.com>
2022-09-19 22:37:38 -0500

    Add files via upload

---
+++ Dividir constructor/Dividir constructor/DIvi.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dividir_constructor
+{
+  
+    internal class Divicionclass
+    {
+        //campo propiedades, como es
+        private double n1;
+        private double n2;
+
+        //constructor
+        public Divicionclass(double n1, double n2)
+        {
+            this.n1 = n1;
+            this.n2 = n2;
+        }
+
+  
+        public double Dividir()
+        {
+            return (n1 / n2);
+        }
+    }
+}
